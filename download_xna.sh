f7b88955571d51ad96cf030fb49fcfeac4c3c8ce
Justyn <egot@istic.pw>
2018-02-24 01:52:09 -0600

    Use shared dlls instead of redist so that we actually have headers

--- download_xna.sh
+++ download_xna.sh
@@ -4,12 +4,18 @@
 mkdir -p bin/XNA
 wget "https://github.com/DDReaper/XNAGameStudio/raw/master/MXA%20Studio/XNA%20Game%20Studio%204.0.6%20(2017).zip" -O XNA.zip
 unzip XNA.zip -d tmpXNA
-cd tmpXNA/"2. XNA Framework 4.0 Redistribution"
-7z e "XNA Framework 4.0 Redist.msi"
-for f in *_dll_GAC
+cd tmpXNA/"4. XNA Game Studio 4.0 Shared"
+7z e "XNA Game Studio Shared.msi"
+7z e "product.cab"
+for f in *_dll
 do
-	fc="$(echo $f | sed -e 's/_GAC//g' -e 's/_/./g')"
-	cp "$f" "../../bin/XNA/$fc"
+        fc="$(echo $f | sed -e 's/_/./g')"
+        cp "$f" "../../bin/XNA/$fc"
+done
+for f in *_xml
+do
+        fc="$(echo $f | sed -e 's/_/./g')"
+        cp "$f" "../../bin/XNA/$fc"
 done
 cd ../..
-rm -rf tmpXNA XNA.zip
+rm -rf bin/XNA/XnaMediaHelper.dll tmpXNA XNA.zip
